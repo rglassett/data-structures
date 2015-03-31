4184cfe19e9dff2a57bfc6268e5316bdb4930a9d
Ryan Glassett <rglassett@gmail.com>
2015-03-31 11:38:34 -0700

    Add #delete to BSTNode

--- binary_search_tree/lib/binary_search_tree.rb
+++ binary_search_tree/lib/binary_search_tree.rb
@@ -1,17 +1,46 @@
 class BSTNode
+  attr_accessor :parent
   attr_reader :comparator, :key, :left, :right
 
-  def initialize(key = nil, &comparator)
+  def initialize(key = nil, parent = nil, &comparator)
     comparator ||= Proc.new { |x, y| x <=> y }
     @comparator = comparator
+    @parent = parent
     @key = key
-    unless key.nil?
-      @left = BSTNode.new(&comparator)
-      @right = BSTNode.new(&comparator)
+    unless leaf?
+      @left = BSTNode.new(nil, self, &comparator)
+      @right = BSTNode.new(nil, self, &comparator)
     end
   end
 
-  def delete
+  def delete(delete_key)
+    return if leaf?
+    if delete_key < key
+      left.delete(delete_key)
+    elsif delete_key > key
+      right.delete(delete_key)
+    else
+      if left.key && right.key
+        # replace with next successor (min of right branch)
+        successor = right.find_min
+        @key = successor.key
+        successor.delete(successor.key)
+      elsif left.key
+        replace_in_parent(left)
+      elsif right.key
+        replace_in_parent(right)
+      else
+        replace_in_parent
+      end
+    end
+  end
+
+  def find_min
+    if left.key
+      left.find_min
+    else
+      self
+    end
   end
 
   def insert(new_key)
@@ -26,6 +55,16 @@ class BSTNode
     key.nil?
   end
 
+  def left=(node)
+    @left = node
+    node && node.parent = self
+  end
+
+  def right=(node)
+    @right = node
+    node && node.parent = self
+  end
+
   def recursive_insert(new_key)
     case comparator.call(new_key, key)
     when -1
@@ -35,15 +74,24 @@ class BSTNode
     end
   end
 
-  def search
+  def replace_in_parent(node = self.class.new(nil, parent, &comparator))
+    return unless parent
+    if parent.left == self
+      parent.left = node
+    else
+      parent.right = node
+    end
+  end
+
+  def search(search_key)
+    return nil if leaf?
+    return self if key == search_key
+    left.search(search_key) || right.search(search_key)
   end
 
   def key=(new_key)
     @key = new_key
-    @left ||= BSTNode.new(&comparator)
-    @right ||= BSTNode.new(&comparator)
+    @left ||= BSTNode.new(nil, self, &comparator)
+    @right ||= BSTNode.new(nil, self, &comparator)
   end
-end
-
-class BinarySearchTree
 end
--- binary_search_tree/spec/binary_search_tree_spec.rb
+++ binary_search_tree/spec/binary_search_tree_spec.rb
@@ -1,7 +1,8 @@
 require 'rspec'
 require 'binary_search_tree'
+require 'byebug'
 
-describe BinarySearchTree do
+describe BSTNode do
   describe "#initialize" do
     context "when given options" do
       let!(:prc) { Proc.new { |x, y| y <=> x } }
@@ -75,6 +76,110 @@ describe BinarySearchTree do
 
           expect(node.right.key).to eq(1)
         end
+      end
+    end
+  end
+
+  describe "#search" do
+    let(:search_tree) { BSTNode.new(10) }
+
+    before(:example) do
+      search_tree.insert(5)
+      search_tree.insert(7)
+      search_tree.insert(15)
+      search_tree.insert(12)
+      search_tree.insert(20)
+    end
+
+    it "returns nil if the key is not found" do
+      expect(search_tree.search(1)).to be_nil
+    end
+
+    it "returns the node if the key is found" do
+      result = search_tree.search(7)
+
+      expect(result).to be_a(BSTNode)
+      expect(result.key).to eq(7)
+    end
+  end
+
+  describe "#delete" do
+    let!(:search_tree) { BSTNode.new(10) }
+    let(:node_five) { search_tree.search(5) }
+    let(:node_seven) { search_tree.search(7) }
+    let(:node_fifteen) { search_tree.search(15) }
+    let(:node_twelve) { search_tree.search(12) }
+    let(:node_twenty) { search_tree.search(20) }
+
+    before(:example) do
+      search_tree.insert(5)
+      search_tree.insert(7)
+      search_tree.insert(15)
+      search_tree.insert(12)
+      search_tree.insert(20)
+    end
+
+    #   10
+    #   |
+    #  -----
+    #  |   |
+    #  5   15
+    #  |   |
+    # ---  -----
+    # | |  |   |
+    #   7  12  20
+
+    context "when deleting a node with no children" do
+      it "removes the node" do
+        expect(search_tree.search(7).key).to eq(7)
+
+        search_tree.delete(7)
+
+        expect(search_tree.search(7)).to be_nil
+      end
+    end
+
+    context "when deleting a node with one child" do
+      it "replaces itself in the parent" do
+        expect(search_tree.left).to be(node_five)
+
+        search_tree.delete(5)
+
+        expect(search_tree.left).to be(node_seven)
+      end
+
+      it "sets the parent of the replacement" do
+        expect(node_seven.parent).to be(node_five)
+
+        search_tree.delete(5)
+
+        expect(node_seven.parent).to be(search_tree)
+      end
+    end
+
+    context "when deleting a node with two children" do
+      it "deletes the key from the tree" do
+        expect(search_tree.search(15)).not_to be_nil
+
+        search_tree.delete(15)
+
+        expect(search_tree.search(15)).to be_nil
+      end
+
+      it "sets its key to that of its next successor" do
+        node_fifteen = search_tree.search(15)
+        expect(node_fifteen.key).to eq(15)
+
+        search_tree.delete(15)
+
+        expect(node_fifteen.key).to eq(20)
+      end
+
+      it "removes the next successor from the tree" do
+        node_fifteen
+        search_tree.delete(15)
+
+        expect(node_fifteen.right).to be_leaf
       end
     end
   end
